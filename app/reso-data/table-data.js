6f010a8d644077208d6a4cf4d3f6932ad33876a5
Sonny <richardhughesjr@gmail.com>
2018-02-24 13:14:49 -0600

    Adds server logic and reso-data table data

---
+++ app/reso-data/table-data.js
@@ -0,0 +1,24 @@
+var tableArray = [
+    {
+        customerName: "Ernest Hemingway",
+        customerPhone: "(123) 456-7890"
+    },
+    {
+        customerName: "Stephen King",
+        customerPhone: "(098) 765-4321"
+    },
+    {
+        customerName: "Mark Twain",
+        customerPhone: "(345) 678-9012"
+    },
+    {
+        customerName: "William Faulkner",
+        customerPhone: "(789) 012-3456"
+    },
+    {
+        customerName: "Charles Dickens",
+        customerPhone: "(567) 890-1234"
+    }
+];
+
+module.exports = tableArray;
