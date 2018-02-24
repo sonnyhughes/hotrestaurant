50fcd6c0767a477dc8622519d241b8e3aea9aad5
Sonny <richardhughesjr@gmail.com>
2018-02-24 12:21:22 -0600

    Adds JSON packages, server.js, and required npm modules to server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+// DEPENDENCIES
+var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+
+// CONFIG EXPRESS
+var app = express();
+var PORT = process.env.PORT || 3000; 
